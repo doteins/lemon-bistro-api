83fe3440efc58429f81957c69856edb73f9b797d
doteins <112283864+doteins@users.noreply.github.com>
2023-07-19 21:22:00 +0300

    Add initial database config

--- core/settings.py
+++ core/settings.py
@@ -81,6 +81,25 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'MySQL': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'my_database',   
+        'USER': 'root',   
+        'PASSWORD': 'your_password',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        } 
+    },
+    "PostgreSQL": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "littlelemon",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
