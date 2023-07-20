c39b78b87a8206de495c5cdb35667441207a2743
doteins <112283864+doteins@users.noreply.github.com>
2023-07-20 11:30:10 +0300

    feat: Add Menu and Booking models and register both of them in admin.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+   name = models.CharField(max_length=200) 
+   price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True, null=False)
+   inventory = models.IntegerField()
+   menu_item_description = models.TextField(max_length=1000, default='') 
+
+   def __str__(self):
+      return self.name
+
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    guests_number = model.SmallIntegerField(default=1)
+    reservation_date = models.DateField()
+    # reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return f'{self.title} : {str(self.price)}'
