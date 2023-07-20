0ea9ab1df5e8e118a0f1e2a6f25d8472becd4c9e
doteins <112283864+doteins@users.noreply.github.com>
2023-07-20 20:56:44 +0300

    feat: Add MenuDetail and BookingViewSet views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html', {})
+
+class MenuView(generics.ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = menuSerializer
+
+class MenuDetailView(generics.RetrieveUpdateAPIView, DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = menuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+  permission_classes = [IsAuthenticated]
+  queryset = Booking.objects.all()
+  serializer_class = bookingSerializer
