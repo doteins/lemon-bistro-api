e681e7461fb33915b8404d8f630aed29d63176d0
doteins <112283864+doteins@users.noreply.github.com>
2023-07-22 11:25:26 +0300

    Implement and update str method for all the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,17 +11,16 @@ class Menu(models.Model):
 	# 	db_table = 'Menu'
 
 	def __str__(self):
-		return self.name
+		return f'{self.name} : {str(self.price)}'
 
 class Booking(models.Model):
 	name = models.CharField(max_length=200)
 	guests_number = models.SmallIntegerField(default=1)
 	reservation_date = models.DateField()
-	# reservation_slot = models.SmallIntegerField(default=10)
 
 	# Change the table's name
 	# class Meta:
 	# 	db_table = 'Booking'
 
-	def __str__(self): 
-		return f'{self.title} : {str(self.price)}'
+	def __str__(self) -> str:
+		return self.name
