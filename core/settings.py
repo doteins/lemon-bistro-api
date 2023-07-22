f2d3c72e21840c72acc615a5993ef7068fde070f
doteins <112283864+doteins@users.noreply.github.com>
2023-07-22 12:54:21 +0300

    Set DRF and djoser config
    DRF: Add render clases, filters, trhottle, pagination and authentication classes for the API. Djoser: Set email as id field for login

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,33 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication"
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        'anon': '5/minute', # This is an example!
+        'user': '50/minute' # This is an example!
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
+
+DJOSER={
+    "USER_ID_FIELD":"username",
+    "LOGIN_FIELD": "email"
+    }
