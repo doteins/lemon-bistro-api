36712d9bfe4ddf5325b6b39f7b7e143dfa3a5f07
doteins <112283864+doteins@users.noreply.github.com>
2023-07-22 11:27:51 +0300

    Implement DefaultRouter and create URLs for booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [ 
     path('', views.index, name="home"),
     path('menu/', views.MenuView.as_view(), name='menu'),
-    path('menu-item/<int:pk>', views.MenuDetailView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.MenuDetailView.as_view(), name='menu'),
     path('api-token-auth', obtain_auth_token)
 ]
